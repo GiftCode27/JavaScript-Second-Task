11cf26e1e093929ae5480dc28d90dab9f296f287
GiftCode27 <105789010+GiftCode27@users.noreply.github.com>
2022-06-21 22:25:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var nameString = function (name) {return "Hello, I am" + " " +  name;};
+var name= nameString ("Gift")
+nameString ("Gift"); alert (name);
+
+var heightString = function (height) {return "I am" + " " + height + " " + "tall";};
+var height = heightString ("187cm") 
+ nameString("187"); alert (height);
+                                 
+var countryString = function(country) {return "I am from" + " " + country;};
+var country = countryString ("Nigeria")  
+nameString ("Nigeria"); alert (country);                                
